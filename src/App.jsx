469d95279c349aacbc3449ae075edd7f7db78ebc
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2026-01-22 09:37:52 -0500

    Update the styles for the contact section and the style comments for all sections

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Inicio from './pages/Inicio';
 import PrivacyPolicy from './pages/PrivacyPolicy';
+import TermsAndConditions from './pages/TermsAndConditions';
 
 const App = () => (
   <Suspense fallback={<div>Cargando...</div>}>
     <Routes>
       <Route path="/" element={<Inicio />} />
       <Route path="/privacidad" element={<PrivacyPolicy />} />
+      <Route path="/terminos" element={<TermsAndConditions />} />
     </Routes>
   </Suspense>
 );
